8639fe325d2c532204b0a5a7148efe38be61a5f3
Leonardo Ramos <103533833+leocapeccir@users.noreply.github.com>
2024-11-10 19:41:06 -0300

    GITHUBG PAGES

---
+++ src/pages/home/index.js
@@ -0,0 +1,43 @@
+import { Container, Movie, MovieList } from "./styles";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+var APIKey = 'f183bca038595fa5d2a8cfb03065bb35';
+const image_path = 'https://image.tmdb.org/t/p/w500'
+
+function Home() {
+
+
+const [movies, setMovies] = useState([])
+useEffect(()=> {
+    
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
+    .then(response => response.json())
+    .then(data => setMovies(data.results))
+    
+}, [])
+
+    
+    return (
+        <Container>
+            <h1>The top 20 most-watched movies</h1>
+        
+            <MovieList>
+
+                {movies.map(movie => {
+                    return (
+                        <Movie key= {movie.id}>
+                            <Link to={`/details/${movie.id}`}><img src={`${image_path}${movie.poster_path}`} alt="spiderman"/></Link>
+                            <span>{movie.title}</span>
+                        </Movie>
+                    )
+                })}
+
+             
+            </MovieList>
+        </Container>
+    )
+}
+
+export default Home;
